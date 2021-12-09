085259ca92ca6ddf524c6c3fa62dc47d468408cb
MSmaga <msmaga@protonmail.com>
2021-12-09 13:00:30 +0200

    Added namespace, Program, Main for VS2019

--- Task_Additional_Notebok/Program.cs
+++ Task_Additional_Notebok/Program.cs
@@ -1,17 +1,27 @@
-﻿#region Задание Дополнительное
-// Используя Visual Studio, создайте проект по шаблону Console Application. 
-// Создайте структуру с именем - Notebook. 
-// Поля структуры: модель, производитель, цена.
-// В структуре должен быть реализован конструктор для инициализации полей и метод для вывода 
-// содержимого полей на экран.
-#endregion
+﻿namespace Task_Additional_Notebok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Задание Дополнительное
+            // Используя Visual Studio, создайте проект по шаблону Console Application. 
+            // Создайте структуру с именем - Notebook. 
+            // Поля структуры: модель, производитель, цена.
+            // В структуре должен быть реализован конструктор для инициализации полей и метод для вывода 
+            // содержимого полей на экран.
+            #endregion
 
 
-// Instance the structure Notebook.
-Notebook notebook = new Notebook("P70", "Lenovo", 1399.0m);
+            // Instance the structure Notebook.
+            Notebook notebook = new Notebook("P70", "Lenovo", 1399.0m);
 
-// Call method of the structure.
-notebook.Show();
+            // Call method of the structure.
+            notebook.Show();
 
-//Delay.
-Console.ReadLine();
+            //Delay.
+            Console.ReadLine();
+        }
+    }
+}
+
